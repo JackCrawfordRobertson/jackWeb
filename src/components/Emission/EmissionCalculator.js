d26f7c4875618c2badedc9462007489e0af14e83
Jack Robetson <jackrobertsonps3@gmail.com>
2023-09-18 13:10:05 +0100

    Emmision calculator

--- src/components/Emission/EmissionCalculator.js
+++ src/components/Emission/EmissionCalculator.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import './badge-styles.css';
 
 function EmissionCalculator() {
-  useEffect(() => {
-    // Load the carbon badge script
-    const script = document.createElement('script');
-    script.src = 'https://unpkg.com/website-carbon-badges@1.1.3/b.min.js';
-    script.defer = true;
-    document.getElementById('wcb').appendChild(script);
-  }, []);
-
-  return <div id="wcb" class="carbonbadge wcb-d"></div>;
+  return (
+    <div className="carbon-button">
+      <div className="carbon-text">
+        <a href="https://www.websitecarbon.com/website/jack-robertson-co-uk/" target="_blank" rel="noopener noreferrer">
+          0.10g of CO2/view
+        </a>
+        <a href="https://www.websitecarbon.com/" target="_blank" rel="noopener noreferrer" className="website-carbon">
+          Website Carbon
+        </a>
+      </div>
+      <div className="carbon-rating">
+        <p>Cleaner than 90% of pages tested</p>
+      </div>
+    </div>
+  );
 }
+
 
 export default EmissionCalculator;
