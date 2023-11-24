bdfb78b09ef80b551440d16c06176bab58de8146
Jack Robetson <jackrobertsonps3@gmail.com>
2023-11-24 19:31:03 +0000

    Build Fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,24 @@
-//Dependencies
-import * as React from "react"
-import { BrowserRouter } from 'react-router-dom';
+// Dependencies
+import * as React from "react";
+import { Link } from 'gatsby'; // Keep this if you're using Gatsby's Link component in your project
 
-//Components
-import Seo from "../components/seo"
-import HomeTop from "../components/HomeTop/HomeTop"
+// Components
+import Seo from "../components/seo";
+import HomeTop from "../components/HomeTop/HomeTop";
 
-//Styles
-import "../styles/global.css"
+// Styles
+import "../styles/global.css";
 
 const Index = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Seo 
-          title="Home" 
-          description="Exploring how storytelling and narratives can unlock insights into complex systems like economics and politics."
-        />
-        <HomeTop/>
-      </div>
-    </BrowserRouter>
-  )
-}
+    <div>
+      <Seo 
+        title="Home" 
+        description="Exploring how storytelling and narratives can unlock insights into complex systems like economics and politics."
+      />
+      <HomeTop />
+    </div>
+  );
+};
 
-export default Index
+export default Index;
