353b72f037242e9ba4520349b9a12077b375d8fd
Jack Robetson <jackrobertsonps3@gmail.com>
2024-04-15 18:00:05 +0100

    CV Page Updates

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -3,16 +3,15 @@ import { Helmet } from "react-helmet";
 
 const CVPage = () => {
   return (
-    <div style={{ height: "100vh", overflow: "hidden" }}>
+    <div style={{ height: "100vh", overflow: "hidden", backgroundColor: 'white' }}>
       <Helmet>
         <title>CV - Jack Robertson</title>
       </Helmet>
-      {/* Ensure the PDF path is correct and publicly accessible */}
-      <embed
-        src="/cv.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
+      {/* Using an img tag to display the image */}
+      <img
+        src="/cv.png"  // Make sure this path is correct based on your static directory configuration
+        style={{ width: "100vw", height: "auto" }}
+        alt="CV of Jack Robertson"
       />
     </div>
   );
