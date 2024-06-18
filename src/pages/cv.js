aeb4e81535e170182fc0ff6613540f9f380f0dfd
Jack Robetson <jackrobertsonps3@gmail.com>
2024-06-18 14:54:14 +0100

    CV page updates

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from 'react';
 
-const CVPage = () => {
-  return (
-    <div style={{ height: "100vh", overflow: "hidden", backgroundColor: 'white' }}>
-      <Helmet>
-        <title>CV - Jack Robertson</title>
-      </Helmet>
-      {/* Using an img tag to display the image */}
-      <img
-        src="/cv.png"  // Make sure this path is correct based on your static directory configuration
-        style={{ width: "100vw", height: "auto" }}
-        alt="CV of Jack Robertson"
-      />
-    </div>
-  );
+const CV = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+            width: '100vw',
+            margin: 0,
+            padding: 0,
+            overflow: 'hidden'
+        }}>
+            <iframe
+                src="/static/JackCRobertson-CV.pdf"
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    border: 'none'
+                }}
+            />
+        </div>
+    );
 };
 
-export default CVPage;
+export default CV;
