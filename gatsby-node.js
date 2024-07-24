274e150ca7ab4dbf937eb5cc6fb5f7eacb58ae69
Jack Robetson <jackrobertsonps3@gmail.com>
2024-07-24 10:13:14 +0100

    CV Update

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = ({ actions }) => {
     const { createRedirect } = actions;
     createRedirect({
         fromPath: `/cv`,
-        toPath: `https://drive.google.com/file/d/1g_bDzoelhFeQScVehBL2EC4tbaGt6Djd/view?usp=sharing`,
+        toPath: `https://drive.google.com/file/d/1MsGR8oBWlljBy9C-cqQIoX2gpd_nrEct/view?usp=sharing`,
         isPermanent: true,
         redirectInBrowser: true,
     });
